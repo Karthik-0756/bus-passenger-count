6fdbb3ed830d8883cef0eebf9bafa660ecfd61e9
Karthik-0756 <karthikmani0756@gmail.com>
2025-02-11 18:41:43 +0530

    second

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/bus_passenger_count')
-  .then(() => console.log('Connected!'));
+// Replace <username>, <password>, and <dbname> with your actual MongoDB Atlas credentials and database name
+const uri = 'mongodb+srv://karthik:Karthik%400756@cluster0.zxheq.mongodb.net/Atlas';
+
+mongoose.connect(uri)
+  .then(() => console.log('Connected to MongoDB Atlas!'))
+  .catch(err => console.error('Error connecting to MongoDB Atlas:', err));
